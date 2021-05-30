2f6fed1845d35395971b9ff116c8019e5c8aa73c
Alikhan <73962071@continental.edu.pe>
2021-05-30 16:40:44 -0500

    give vehicle a forward motion + obstacles

---
+++ Lesson1_1/ControlPlayerMoveCar/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move the vehicle forward
+        //transform.Translate(0, 0, 1); //move possition on z direction
+        //transform.Translate(Vector3.forward); //move possition on z direction
+        transform.Translate(Vector3.forward * Time.deltaTime * 20); //move possition on z direction with 20 meters per second
+    }
+}
